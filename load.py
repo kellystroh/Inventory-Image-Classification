1df6525807856f58a6243aa082fb3ef2a1bdb371
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-22 10:22:03 -0700

    create data cleaning notebook for initial processing; update gitignore for ipynb_checkpoints; code changes to avoid warning in load.py

--- load.py
+++ load.py
@@ -30,8 +30,8 @@ for i, ix in enumerate( df.id ):
         all_img.append( [0]*(60*80*3) )
 
 all_img = np.stack(all_img)
-bw_img = all_img.reshape(-1, 80, 60, 3).mean(3).reshape(-1, 80*60).astype('float16')
-bw_img = np.round(bw_img, 1)
+bw_float64 = all_img.reshape(-1, 80, 60, 3).mean(3).reshape(-1, 80*60)
+bw_img = bw_float64.astype('float16')
 
 ### find bad images
 bad_rows_idx = np.where(bw_img.sum(axis=1)==0)[0].tolist()
@@ -39,10 +39,12 @@ every_row_idx = list(range(44446))
 keep_rows_idx = list(set(every_row_idx)-set(bad_rows_idx))
 
 ### exclude bad images
-bw_img = bw_img[keep_rows_idx, :]
+X1 = all_img[keep_rows_idx, :]
+X2 = bw_img[keep_rows_idx, :]
 
-df = df[~df.index.isin(bad_rows_idx)].reset_index()
+df = df[~df.index.isin(bad_rows_idx)].reset_index(drop=True)
 
-df.to_csv('data/labels_df.csv',index=False)
-
-np.savez_compressed('data/image_array', a=all_img, b=bw_img)
+df.to_csv('data/labels_df.csv', index=False)
+print('df saved')
+np.savez_compressed('data/image_array', a=X1, b=X2)
+print('array saved')
