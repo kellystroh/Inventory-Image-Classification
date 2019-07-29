3a771ca6d4d4fa7e773fc143972ad17ee301eaf9
Ubuntu <ubuntu@ip-172-31-40-137.us-east-2.compute.internal>
2019-07-29 17:41:35 +0000

    add model functions py file for easy import of functions used with every model

---
+++ model_functions.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+
+
+
+def pick_ylabels(column):
+    y_train = df_train[column].copy().astype('category').cat.codes
+    y_test = df_test[column].copy().astype('category').cat.codes
+    return (y_train.values, y_test.values)
+def multi_index_counts(col, col2):
+    counts = df_test.groupby([col, col2]).count().id
+    return counts
+
+def category_codes(column):
+    _, ytest = pick_ylabels(column)
+    cat_codes = {}
+    cat_code_list = []
+    for i in range(len(df_test[column].value_counts().index)):
+        s = i
+        t = df_test[column].value_counts().index[i]
+        cat_codes[s] = t
+    return cat_codes
+
+def test_counts_by_cat(column):
+    _, ytest = pick_ylabels(column)
+    test_counts_dict = {}
+    test_counts = []
+    for i in range(len(df_test[column].value_counts().index)):
+        s = df_test[column].value_counts().values[i]
+        t = df_test[column].value_counts().index[i]
+        test_counts_dict[t] = s
+        test_counts.append([t, s])
+    return test_counts
+
+def train_counts_by_cat(column):
+    y_train = pick_ylabels(column)
+    train_counts_dict = {}
+    train_counts = []
+    for i in range(len(df_test[column].value_counts().index)):
+        s = df_train[column].value_counts().values[i]
+        t = df_train[column].value_counts().index[i]
+        train_counts_dict[t] = s
+        train_counts.append([t, s])
+    return test_counts
+
+def class_weights(column):
+    train_counts = train_counts_by_cat(column)
+    counts_list = []
+    ratio_list = []
+    ratio_dict = {}
+    for x in test_counts:
+        counts_list.append(x[1])
+    z = sum(counts_list)
+    for x in counts_list:
+        if np.round(x/z, 3) ==0:
+            ratio_list.append(.001)
+        else: 
+            ratio_list.append(np.round(x/z, 2))
+    for k, v in enumerate(ratio_list):
+        ratio_dict[k] = v
+    return ratio_dict
+def model_results(model, column):
+    yhat = model.predict(X_test)
+    _ , ytest = pick_ylabels(column)
+    accuracy = accuracy_score(ytest, yhat)
+    print( 'Accuracy Score: ', accuracy )
+    recall = recall_score(ytest, yhat, average='weighted')
+    print( 'Recall Score: ', recall)
+    y_proba = model.predict_proba(X_test)
+    wrong_id_list = []
+    pred_cat_list = []
+    real_cat_list = []
+    for row_idx in range(len(ytest)):
+        if ytest[row_idx]!=yhat[row_idx]:
+            wrong_id_list.append(row_idx)
+            pred_cat_list.append(yhat[row_idx])
+            real_cat_list.append(ytest[row_idx])
+
+    arr = np.array([ real_cat_list, pred_cat_list])
+    arr = arr.transpose()
+    wrong_df = pd.DataFrame( arr, index= wrong_id_list, columns = ['actual', 'predicted'] )
+    return (accuracy, recall, wrong_df)
+
